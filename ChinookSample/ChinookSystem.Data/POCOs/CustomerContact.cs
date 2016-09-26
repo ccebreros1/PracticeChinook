f74f4c4d09860245861341f4df2ec6f0a89f8226
ccebreros1 <cesar.cebreros10@gmail.com>
2016-09-26 17:03:22 -0600

    Added CustomerContact POCO class

---
+++ ChinookSample/ChinookSystem.Data/POCOs/CustomerContact.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//No annotations since this is not an entity
+
+namespace ChinookSystem.Data.POCOs
+{
+    public class CustomerContact
+    {
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+
+    }
+}
