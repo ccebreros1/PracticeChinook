feb6dd59d67c23a3ab8927b910a59943bac9197e
ccebreros1 <cesar.cebreros10@gmail.com>
2016-10-18 16:08:42 -0600

    Security classes to display data

---
+++ ChinookSample/ChinookSystem/Security/UnregisteredUserProfile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChinookSystem.Security
+{
+    public enum UnregisteredUserType { Undefined, Employee, Customer }
+    public class UnregisteredUserProfile
+    {
+        public int UserId { get; set; } //Generated
+        public string UserName { get; set; }//Collected
+        public string FirstName { get; set; } //From User table
+        public string LastName { get; set; } //From User table
+        public string Email { get; set; } //Collected
+        public UnregisteredUserType UserType { get; set; }
+
+    }
+}
